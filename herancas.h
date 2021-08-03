487b9d63c6ca071e314a3dd711773214b9b0f471
Valdomiro Santos <valdomiro.jose@outlook.com>
2021-08-03 16:44:40 -0300

    Herança multipla

---
+++ herancas.h
@@ -0,0 +1,28 @@
+#ifndef HERANCAS_H_INCLUDED
+#define HERANCAS_H_INCLUDED
+
+class Base1 {
+    public:
+        void impBase1();
+};
+
+void Base1::impBase1() {
+    std::cout << "Imp classe Base1" << std::endl;
+}
+
+
+class Base2 {
+    public:
+        void impBase2();
+};
+
+void Base2::impBase2() {
+    std::cout << "Imp classe Base2" << std::endl;
+}
+
+
+class CFB:public Base1, public Base2 {
+
+};
+
+#endif
