b5772a501584d4fcb6dcf219a1c7b548828061a0
Valdomiro Santos <valdomiro.jose@outlook.com>
2021-08-04 04:18:02 -0300

    Inicializacao uniforme e for simplificado

---
+++ aula25_inicializacao_uniforme.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    system("clear");
+
+    // Basicamente é usar {} ao inves de = para atribuir valores às variaveis
+
+    int num{10};
+    string nome{"Bruno"};
+    vector <int> valores{1, 2, 3, 4, 5};    
+
+    cout << num << endl;
+    
+}
---
+++ aula26_for_mais_simples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    system("clear");
+
+    int x[10]{503, 12, 32, 352, 234, 552, 662, 797, 86, 609,};
+
+    for (int i:x) {  // Serve apenas para iteraveis 
+        cout << i << endl;
+    }
+}
